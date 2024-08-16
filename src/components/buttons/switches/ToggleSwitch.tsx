36f39113561c860e4c5d2b1d093d30cabcab997f
Camila <efcamilaweb@gmail.com>
2024-08-16 14:29:13 -0300

    Arreglo: posición de los íconos

--- src/components/buttons/switches/ToggleSwitch.tsx
+++ src/components/buttons/switches/ToggleSwitch.tsx
@@ -19,23 +19,22 @@ const ToggleSwitch = ({
   return (
     <div
       onClick={onChange}
-      className="flex w-20 h-11 rounded-full justify-evenly relative items-center bg-gray-200 shadow-sm cursor-pointer"
+      className="flex justify-center gap-2 w-[70px] h-9 rounded-full relative items-center bg-gray-200 shadow-sm cursor-pointer"
     >
       <span
         className={`${
-          value === true ? "ml-10" : "ml-2"
+          value === true ? "ml-[2.2rem]" : "ml-1"
         } ${color} h-8 w-8 rounded-full transition-all duration-300 flex items-center justify-center absolute left-0`}
       ></span>
-      <span className="flex justify-center items-center z-20">
-        {icons?.left && (
+      <span className="flex justify-center items-center z-20 mr-1">
+        {icons?.left &&
           React.cloneElement(icons?.left, {
             className: `w-[21px] h-[21px] ${
               value === true ? "text-gray-400" : "text-white"
             }`,
-          })
-        )}
+          })}
       </span>
-      <span className="flex justify-center items-center z-20">
+      <span className="flex justify-center items-center z-20 ">
         {icons?.right &&
           React.cloneElement(icons?.right, {
             className: `w-[21px] h-[21px] ${
