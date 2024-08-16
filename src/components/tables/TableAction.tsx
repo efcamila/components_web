04193a3a19ecfcd9bfe6376553d86a97738a87a1
Camila <efcamilaweb@gmail.com>
2024-08-16 14:30:27 -0300

    Prueba de modal en íconos de una tabla

--- src/components/tables/TableAction.tsx
+++ src/components/tables/TableAction.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from "react";
 import { GoPencil } from "react-icons/go";
 import { IoEyeOutline, IoTrashOutline } from "react-icons/io5";
+import { Button } from "../buttons/clasic/Button";
 
 interface BaseTableProps {
   children: ReactNode;
@@ -12,17 +13,11 @@ interface TableHeadProps {
 }
 
 interface ActionsTableRowProps extends BaseTableProps {
-  actions: {
-    view?: string;
-    edit?: string;
-    delete?: string;
-  };
-  id: number;
+  onClick: (action: "view" | "edit" | "delete") => void;
 }
 
 interface NoActionsTableRowProps extends BaseTableProps {
-  actions?: undefined;
-  id?: null;
+  onClick?: undefined;
 }
 
 type TableRowProps = ActionsTableRowProps | NoActionsTableRowProps;
@@ -33,7 +28,7 @@ interface TableCellProps {
 
 const Table = ({ children }: BaseTableProps) => {
   return (
-    <div className="overflow-auto w-full max-w-[1200px] bg-gray-50 border border-gray-200 rounded-lg p-4 dark:bg-gray-900 dark:border-gray-800">
+    <div className="overflow-auto w-full max-w-[1200px] bg-gray-50 border border-gray-200 rounded-lg p-4 dark:bg-black-700 dark:border-black-800">
       <table className="w-full max-w-[1200px] border-collapse">
         {children}
       </table>
@@ -43,11 +38,11 @@ const Table = ({ children }: BaseTableProps) => {
 
 const TableHead = ({ children, actions = false }: TableHeadProps) => {
   return (
-    <thead className="bg-gray-100 rounded-lg dark:bg-gray-800">
+    <thead className="bg-gray-100 rounded-lg dark:bg-black-800">
       <tr>
         {children}
         {actions && (
-          <th className="text-start h-10 px-3 py-2 text-nowrap font-bold text-xs text-gray-500 last:rounded-r-lg uppercase w-[50px] dark:text-gray-300">
+          <th className="text-start h-10 px-3 py-2 text-nowrap font-bold text-xs text-gray-500 last:rounded-r-lg uppercase w-[50px] dark:text-black-100">
             Acciones
           </th>
         )}
@@ -58,41 +53,44 @@ const TableHead = ({ children, actions = false }: TableHeadProps) => {
 
 const TableColumn = ({ children }: TableHeadProps) => {
   return (
-    <th className="text-start h-10 px-3 py-2 text-nowrap font-bold text-xs text-gray-500 first:rounded-l-lg uppercase dark:text-gray-400">
+    <th className="text-start h-10 px-3 py-2 text-nowrap font-bold text-xs text-gray-500 first:rounded-l-lg uppercase dark:text-black-100">
       {children}
     </th>
   );
 };
 
 const TableBody = ({ children }: BaseTableProps) => {
-  return <tbody className="bg-gray-50 dark:bg-gray-900">{children}</tbody>;
+  return <tbody className="bg-gray-50 dark:bg-black-700">{children}</tbody>;
 };
 
-const TableRow = ({ children, actions, id }: TableRowProps) => {
-  if (actions && typeof id === "undefined") {
-    throw new Error("ID is required when actions are provided.");
-  }
+const TableRow = ({ children, onClick }: TableRowProps) => {
 
   return (
     <tr className="">
       {children}
-      {actions && (
-        <td className="flex justify-center items-center gap-3 h-10 px-3 py-2 text-nowrap first:rounded-l-lg dark:text-gray-100">
-          {actions.view && id && (
-            <a href={`${actions.view}/${id}`}>
-              <IoEyeOutline className="text-xl font-normal text-gray-600 hover:text-gray-800 dark:text-gray-300 dark:hover:text-gray-500" />
-            </a>
-          )}
-          {actions.edit && id && (
-            <a href={`${actions.edit}/${id}`}>
-              <GoPencil className="text-xl text-gray-600 hover:text-gray-800 dark:text-gray-300 dark:hover:text-gray-500" />
-            </a>
-          )}
-          {actions.delete && id && (
-            <a href={`${actions.delete}/${id}`}>
-              <IoTrashOutline className="text-xl text-rose-600 hover:text-rose-800" />
-            </a>
-          )}
+      {onClick && (
+        <td className="flex justify-center items-center gap-3 px-3 py-2 text-nowrap first:rounded-l-lg dark:text-gray-100">
+          <Button
+            type="button"
+            style="neutral"
+            variant="icon"
+            onClick={() => onClick("view")}
+          >
+            <IoEyeOutline className="text-xl font-normal" />
+          </Button>
+
+          <Button
+            type="button"
+            style="neutral"
+            variant="icon"
+            onClick={() => onClick("edit")}
+          >
+            <GoPencil className="text-xl" />
+          </Button>
+
+          <Button type="button" style="danger" variant="icon" onClick={() => onClick("delete") }>
+            <IoTrashOutline className="text-xl" />
+          </Button>
         </td>
       )}
     </tr>
@@ -108,7 +106,17 @@ const TableCell = ({ children }: TableCellProps) => {
 };
 
 const TableCellInfo = ({ children }: TableCellProps) => {
-  return <p className="text-sm text-gray-400 dark:text-gray-500">{children}</p>
+  return (
+    <p className="text-sm text-gray-400 dark:text-black-100">{children}</p>
+  );
 };
 
-export { Table, TableHead, TableBody, TableColumn, TableRow, TableCell, TableCellInfo };
+export {
+  Table,
+  TableHead,
+  TableBody,
+  TableColumn,
+  TableRow,
+  TableCell,
+  TableCellInfo,
+};
