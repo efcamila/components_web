e931724e97d543dc15d074cb0503377dd0f170d9
Camila <efcamilaweb@gmail.com>
2024-09-23 19:37:21 -0300

    Creación componente: Aside

---
+++ src/components/navigation/aside/Aside.tsx
@@ -0,0 +1,31 @@
+import React,{ ReactNode } from "react"
+
+const Aside = ({children}:{children:ReactNode}) => {
+  return (
+    <aside className="flex flex-col gap-8 py-6 pl-16 overflow-y-auto min-h-full">
+        {children}
+    </aside>
+  )
+}
+
+const AsideContent = ({children, title}:{children:ReactNode, title?:string}) => {
+    return (
+      <div className="flex flex-col gap-2">
+        <h5 className="text-sm font-semibold dark:text-gray-200 text-black-800 mb-4">{title}</h5>
+          <ul className="text-sm flex flex-col gap-5">
+          {children}
+          </ul>
+      </div>
+    )
+  }
+
+  
+const AsideItem = ({children}:{children:ReactNode}) => {
+    return (
+      <li className="flex dark:text-gray-200">
+          {children}
+      </li>
+    )
+  }
+
+export {Aside, AsideContent, AsideItem}
