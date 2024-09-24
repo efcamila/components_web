ddd8dd4727b4693f296ce06daee6317927435cb7
Camila <efcamilaweb@gmail.com>
2024-09-23 21:32:44 -0300

    Correcciones

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App'; 
 import './index.css';
 
-// Renderiza la aplicación
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
